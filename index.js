83c463819f98c35f18f19cc369dd6f28b18fe62c
richardgrechko <162537734+richardgrechko@users.noreply.github.com>
2024-07-01 14:33:16 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,23 @@ let pointsDisplay = document.getElementById('points');
 
 document.getElementById('button').addEventListener('click', () => {
         letterIndex++;
-    if (letterIndex < 25) {
-        pointsDisplay.textContent = `Stats: ${alphabet[letterIndex]}`;
+    if (letterIndex < (26**5)-1) {
+        letterIndex++;
+        pointsDisplay.textContent = `Stats: ${alphabet[Math.floor(letterIndex/26)]}${alphabet[letterIndex]}`;
+    }
+    if (letterIndex < (26**4)-1) {
+        letterIndex++;
+        pointsDisplay.textContent = `Stats: ${alphabet[Math.floor(letterIndex/(26**3))-1]}${alphabet[Math.floor(letterIndex/(26**2)) % 26]}${alphabet[Math.floor(letterIndex/26) % 26]}${alphabet[Math.floor(letterIndex) % 26]}`;
+    }
+    if (letterIndex < (26**3)-1) {
+        letterIndex++;
+        pointsDisplay.textContent = `Stats: ${alphabet[Math.floor(letterIndex/(26**2))-1]}${alphabet[Math.floor(letterIndex/26) % 26]}${alphabet[Math.floor(letterIndex) % 26]}`;
     }
     if (letterIndex < (26**2)-1) {
         letterIndex++;
-        pointsDisplay.textContent = `Stats: ${alphabet[Math.floor(letterIndex/26)]}${alphabet[letterIndex]}`;
+        pointsDisplay.textContent = `Stats: ${alphabet[Math.floor(letterIndex/26)-1]}${alphabet[Math.floor(letterIndex) % 26]}`;
+    }
+    if (letterIndex < 25) {
+        pointsDisplay.textContent = `Stats: ${alphabet[Math.floor(letterIndex)]}`;
     }
 });
