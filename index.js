6975fd0871a32d7301dfe1666f6635ecd7bc07f7
richardgrechko <162537734+richardgrechko@users.noreply.github.com>
2024-07-01 14:46:46 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,23 +4,19 @@ let pointsDisplay = document.getElementById('points');
 
 document.getElementById('button').addEventListener('click', () => {
         letterIndex++;
-    if (letterIndex < (26**5)-1) {
-        letterIndex++;
-        pointsDisplay.textContent = `Stats: ${alphabet[Math.floor(letterIndex/26)]}^${alphabet[Math.floor(letterIndex.log(26))]}`;
-    }
-    if (letterIndex < (26**4)-1) {
-        letterIndex++;
-        pointsDisplay.textContent = `Stats: ${alphabet[Math.floor(letterIndex/(26**3))-1]}${alphabet[Math.floor(letterIndex/(26**2)) % 26]}${alphabet[Math.floor(letterIndex/26) % 26]}${alphabet[Math.floor(letterIndex) % 26]}`;
-    }
-    if (letterIndex < (26**3)-1) {
-        letterIndex++;
-        pointsDisplay.textContent = `Stats: ${alphabet[Math.floor(letterIndex/(26**2))-1]}${alphabet[Math.floor(letterIndex/26) % 26]}${alphabet[Math.floor(letterIndex) % 26]}`;
-    }
-    if (letterIndex < (26**2)-1) {
-        letterIndex++;
-        pointsDisplay.textContent = `Stats: ${alphabet[Math.floor(letterIndex/26)-1]}${alphabet[Math.floor(letterIndex) % 26]}`;
-    }
     if (letterIndex < 25) {
         pointsDisplay.textContent = `Stats: ${alphabet[Math.floor(letterIndex)]}`;
+    }
+    if (letterIndex < (26**2)-1) {
+        pointsDisplay.textContent = `Stats: ${alphabet[Math.floor(letterIndex/26)-1]}${alphabet[Math.floor(letterIndex) % 26]}`;
+    }
+    if (letterIndex < (26**3)-1) {
+        pointsDisplay.textContent = `Stats: ${alphabet[Math.floor(letterIndex/(26**2))-1]}${alphabet[Math.floor(letterIndex/26) % 26]}${alphabet[Math.floor(letterIndex) % 26]}`;
+    }
+    if (letterIndex < (26**4)-1) {
+        pointsDisplay.textContent = `Stats: ${alphabet[Math.floor(letterIndex/(26**3))-1]}${alphabet[Math.floor(letterIndex/(26**2)) % 26]}${alphabet[Math.floor(letterIndex/26) % 26]}${alphabet[Math.floor(letterIndex) % 26]}`;
+    }
+    if (letterIndex < (26**5)-1) {
+        pointsDisplay.textContent = `Stats: ${alphabet[Math.floor(letterIndex/(26**Math.floor(letterIndex.log(26))))]}^${alphabet[Math.floor(letterIndex.log(26))]}`;
     }
 });
